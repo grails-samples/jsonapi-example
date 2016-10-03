4d654d73235a14a87240d618912a7253c6e21cca
Colin Harrington <colin.harrington@gmail.com>
2016-10-03 11:57:31 -0500

    Updated internals of the app to a Grails 3.2.0 rest-api profile

--- grails-app/controllers/jsonapi/example/UrlMappings.groovy
+++ grails-app/controllers/jsonapi/example/UrlMappings.groovy
@@ -3,14 +3,15 @@ package jsonapi.example
 class UrlMappings {
 
     static mappings = {
-        "/$controller/$action?/$id?(.$format)?"{
-            constraints {
-                // apply constraints here
-            }
-        }
+        delete "/$controller/$id(.$format)?"(action:"delete")
+        get "/$controller(.$format)?"(action:"index")
+        get "/$controller/$id(.$format)?"(action:"show")
+        post "/$controller(.$format)?"(action:"save")
+        put "/$controller/$id(.$format)?"(action:"update")
+        patch "/$controller/$id(.$format)?"(action:"patch")
 
-        "/"(view:"/index")
-        "500"(view:'/error')
-        "404"(view:'/notFound')
+        "/"(controller: 'application', action:'index')
+        "500"(view: '/error')
+        "404"(view: '/notFound')
     }
 }
